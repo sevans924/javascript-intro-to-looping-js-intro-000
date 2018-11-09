547aaf1283f426d1266a8449f16c2ad74a4879ca
Sarah Elzinga <sevans924@gmail.com>
2018-11-09 22:15:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,13 +22,15 @@ function whileLoop(n) {
 
 function doWhileLoop(array) {
 
-var i = 0
+var i = 0;
 
   function incrementVariable() {
     i = i + 1;
   }
 
   do {
+    console.log('array.length = ' + array.length + ' and i = ' + i);
+    array = array.slice(1);
     incrementVariable();
   } while (array.length > 0 && incrementVariable());
 
